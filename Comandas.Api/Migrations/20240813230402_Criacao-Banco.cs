9f20d71b6f85ca7c4abbd3d22a3bf4e144abfbb1
Diogo Nunes Vicente <diogonunesv742@gmail.com>
2024-08-13 21:17:16 -0300

    criação do banco de dados

---
+++ Comandas.Api/Migrations/20240813230402_Criacao-Banco.cs
@@ -0,0 +1,213 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Comandas.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class CriacaoBanco : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterDatabase()
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "CardapioItems",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Titulo = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Descricao = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Preco = table.Column<decimal>(type: "decimal(65,30)", nullable: false),
+                    PossuiPreparo = table.Column<bool>(type: "tinyint(1)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CardapioItems", x => x.id);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "Comandas",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    NumeroMesa = table.Column<int>(type: "int", nullable: false),
+                    NomeCliente = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    SituacaoComanda = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Comandas", x => x.id);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "Mesas",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    NumeroMesa = table.Column<int>(type: "int", nullable: false),
+                    SituacaoMesa = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Mesas", x => x.id);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "Usuarios",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Nome = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Email = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    senha = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Usuarios", x => x.Id);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "ComandasItem",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    CardapioItemId = table.Column<int>(type: "int", nullable: false),
+                    ComandaId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ComandasItem", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ComandasItem_CardapioItems_CardapioItemId",
+                        column: x => x.CardapioItemId,
+                        principalTable: "CardapioItems",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_ComandasItem_Comandas_ComandaId",
+                        column: x => x.ComandaId,
+                        principalTable: "Comandas",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "pedidoCozinhas",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    ComandaId = table.Column<int>(type: "int", nullable: false),
+                    SituacaoId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_pedidoCozinhas", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_pedidoCozinhas_Comandas_ComandaId",
+                        column: x => x.ComandaId,
+                        principalTable: "Comandas",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "pedidoCozinhaItems",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    PedidoCozinhaId = table.Column<int>(type: "int", nullable: false),
+                    ComandaItemId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_pedidoCozinhaItems", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_pedidoCozinhaItems_ComandasItem_ComandaItemId",
+                        column: x => x.ComandaItemId,
+                        principalTable: "ComandasItem",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_pedidoCozinhaItems_pedidoCozinhas_PedidoCozinhaId",
+                        column: x => x.PedidoCozinhaId,
+                        principalTable: "pedidoCozinhas",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ComandasItem_CardapioItemId",
+                table: "ComandasItem",
+                column: "CardapioItemId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ComandasItem_ComandaId",
+                table: "ComandasItem",
+                column: "ComandaId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_pedidoCozinhaItems_ComandaItemId",
+                table: "pedidoCozinhaItems",
+                column: "ComandaItemId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_pedidoCozinhaItems_PedidoCozinhaId",
+                table: "pedidoCozinhaItems",
+                column: "PedidoCozinhaId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_pedidoCozinhas_ComandaId",
+                table: "pedidoCozinhas",
+                column: "ComandaId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Mesas");
+
+            migrationBuilder.DropTable(
+                name: "Usuarios");
+
+            migrationBuilder.DropTable(
+                name: "pedidoCozinhaItems");
+
+            migrationBuilder.DropTable(
+                name: "ComandasItem");
+
+            migrationBuilder.DropTable(
+                name: "pedidoCozinhas");
+
+            migrationBuilder.DropTable(
+                name: "CardapioItems");
+
+            migrationBuilder.DropTable(
+                name: "Comandas");
+        }
+    }
+}
